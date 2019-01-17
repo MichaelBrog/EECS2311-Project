7a30f2c9c65e5972e50572ca45291b29dd047048
mostafammagdy <mostafa.magdy.moussa@gmail.com>
2019-01-16 18:35:46 -0600

    i changed a couple of stuff for the frame app, buttons haven't showed up, will continue on it in an hour or so.

--- TalkBox/src/SimulatorFrame.java
+++ TalkBox/src/SimulatorFrame.java
@@ -1,7 +1,8 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class SimulatorFrame extends JFrame{
 /**
@@ -15,9 +16,17 @@ public class SimulatorFrame extends JFrame{
 	
 	// -----------------------------------------
 	
-	public SimulatorFrame (ActionListener l, int n) {
-		super("Simulator App");
-		initializePanel(l);
+	public SimulatorFrame (ActionListener l, int n) {	
+		JFrame frame = new JFrame("FrameDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel emptyLabel = new JLabel("Demo frame");
+        emptyLabel.setPreferredSize(new Dimension(1650, 1000));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        
+		
+        frame.pack();
+        frame.setVisible(true);
 	}
 	
 	/**
@@ -26,21 +35,31 @@ public class SimulatorFrame extends JFrame{
 	 * 						--- BorderLayout ? -----
 	 * */
 	private void initializePanel(ActionListener l) {
+	
 		
 	}
+	
+	
 	
 	/**
 	 * play the sound based on the pressed button
 	 * */
-	public void panelUpdate (JButton b) {
+	public static void panelUpdate (JButton b, String buttonName) {
 		
+		b=new JButton(buttonName, new ImageIcon("play.png"));    
+		b.setBounds(100,100,140, 40);
 	}
 	
 	/**
 	 * A main method to test the view of the simulator app
 	 * */
 	public static void main (String[] args) {
+		JButton b1 = null,b2=null,b3=null;
 		
+		new SimulatorFrame(null, 1);
+		panelUpdate(b1, "Happy");
+		panelUpdate(b1, "Sad");
+		panelUpdate(b1, "Angry");
 	}
 	
 	
