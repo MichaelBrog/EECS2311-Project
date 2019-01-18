2d5b2ed5a3fee1accbc725d75196fc1e5f65166a
mostafammagdy <mostafa.magdy.moussa@gmail.com>
2019-01-17 23:18:46 -0600

    made some more changes, couldnt get the buttons to work, changed the background color, learning how to add another color for the top half of the frame

--- TalkBox/src/SimulatorFrame.java
+++ TalkBox/src/SimulatorFrame.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
@@ -20,12 +21,13 @@ public class SimulatorFrame extends JFrame{
 		JFrame frame = new JFrame("FrameDemo");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel emptyLabel = new JLabel("Demo frame");
-        emptyLabel.setPreferredSize(new Dimension(1650, 1000));
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-        
+		frame.setPreferredSize(new Dimension(1100,600));;
+		frame.getContentPane().setBackground(Color.cyan);
+		
+		
 		
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 	}
 	
@@ -46,20 +48,17 @@ public class SimulatorFrame extends JFrame{
 	 * */
 	public static void panelUpdate (JButton b, String buttonName) {
 		
-		b=new JButton(buttonName, new ImageIcon("play.png"));    
-		b.setBounds(100,100,140, 40);
+		
 	}
 	
 	/**
 	 * A main method to test the view of the simulator app
 	 * */
 	public static void main (String[] args) {
-		JButton b1 = null,b2=null,b3=null;
+		
 		
 		new SimulatorFrame(null, 1);
-		panelUpdate(b1, "Happy");
-		panelUpdate(b1, "Sad");
-		panelUpdate(b1, "Angry");
+		
 	}
 	
 	
