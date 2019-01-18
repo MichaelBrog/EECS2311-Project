d379ac760973e235f7e148e7d1d686be009ed4e5
mostafammagdy <mostafa.magdy.moussa@gmail.com>
2019-01-17 23:40:09 -0600

    ..

--- TalkBox/src/SimulatorFrame.java
+++ TalkBox/src/SimulatorFrame.java
@@ -1,65 +1,57 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.*;
 
-public class SimulatorFrame extends JFrame{
-/**
- * A visual representation of simulation app
- * */
-	
+public class SimulatorFrame extends JFrame {
+	/**
+	 * A visual representation of simulation app
+	 */
+
 	// ------------ Fields ---------------------
-	JButton []pics;							// An array of buttons. Size initialized by the desired amount of buttons
-	
-	
-	
+	JButton[] pics; // An array of buttons. Size initialized by the desired amount of buttons
+
 	// -----------------------------------------
-	
-	public SimulatorFrame (ActionListener l, int n) {	
+
+	public SimulatorFrame(ActionListener l, int n) {
 		JFrame frame = new JFrame("FrameDemo");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setPreferredSize(new Dimension(1100, 600));
 		
-		frame.setPreferredSize(new Dimension(1100,600));;
 		frame.getContentPane().setBackground(Color.cyan);
-		
-		
-		
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
-	
+
 	/**
 	 * A method that initialized the panel and provide the first view for the client
-	 * Associate the action listener l to the buttons
-	 * 						--- BorderLayout ? -----
-	 * */
+	 * Associate the action listener l to the buttons --- BorderLayout ? -----
+	 */
 	private void initializePanel(ActionListener l) {
-	
-		
+
 	}
-	
-	
-	
+
 	/**
 	 * play the sound based on the pressed button
-	 * */
-	public static void panelUpdate (JButton b, String buttonName) {
-		
-		
+	 */
+	public static void panelUpdate(JButton b, String buttonName) {
+
 	}
-	
+
 	/**
 	 * A main method to test the view of the simulator app
-	 * */
-	public static void main (String[] args) {
-		
-		
+	 */
+	public static void main(String[] args) {
+
 		new SimulatorFrame(null, 1);
-		
+
 	}
-	
-	
+
 }
