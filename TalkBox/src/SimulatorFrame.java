032991270526d6a78f3656e67582e6a4b0f45505
mostafammagdy <mostafa.magdy.moussa@gmail.com>
2019-01-21 11:42:18 -0600

    change to frame

--- TalkBox/src/SimulatorFrame.java
+++ TalkBox/src/SimulatorFrame.java
@@ -1,56 +1,160 @@
 import java.awt.BorderLayout;
+
 import java.awt.Color;
+
 import java.awt.Component;
+
 import java.awt.Dimension;
+
+import java.awt.GridLayout;
+
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+
 import javax.swing.border.*;
 
 public class SimulatorFrame extends JFrame {
+
 	/**
+	 * 
 	 * A visual representation of simulation app
+	 * 
 	 */
 
 	// ------------ Fields ---------------------
+
 	JButton[] pics; // An array of buttons. Size initialized by the desired amount of buttons
+
+	JPanel panel; // The used panel
 
 	// -----------------------------------------
 
 	public SimulatorFrame(ActionListener l, int n) {
-		JFrame frame = new JFrame("FrameDemo");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		frame.setPreferredSize(new Dimension(1100, 600));
-		
-		frame.getContentPane().setBackground(Color.cyan);
+		super("FrameDemo");
 
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		initializePanel(l, n);
+
+		// frame.getContentPane().setBackground(Color.cyan);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setPreferredSize(new Dimension(1100, 300));
+
+		pack();
+
+		setLocationRelativeTo(null);
+
+		setVisible(true);
+
 	}
 
 	/**
-	 * A method that initialized the panel and provide the first view for the client
-	 * Associate the action listener l to the buttons --- BorderLayout ? -----
+	 * 
+	 * @param n
+	 * 
+	 *          The number of buttons to be declared
+	 * 
+	 * @param l
+	 * 
+	 *          The action listener
+	 * 
+	 * 
+	 * 
+	 *          A method that initialized the panel and provide the first view for
+	 *          the client
+	 * 
+	 *          Associate the action listener l to the buttons --- GridLayout ?
+	 *          -----
+	 * 
 	 */
-	private void initializePanel(ActionListener l) {
+
+	private void initializePanel(ActionListener l, int n) {
+
+		pics = new JButton[n];
+
+		panel = new JPanel(new GridLayout(1, n, 1, 1));
+
+		for (int i = 0; i < pics.length; i++) {
+
+			pics[i] = new JButton("Something");
+
+//			pics[i].setBackground(Color.BLUE);
+			
+			pics[i].setVerticalTextPosition(SwingConstants.TOP);
+			pics[i].setHorizontalTextPosition(SwingConstants.CENTER);
+
+			pics[i].setVisible(true);
+
+			pics[i].addActionListener(l);
+
+			panel.add(pics[i]);
+
+		}
+
+		this.add(panel);
 
 	}
 
 	/**
+	 * 
+	 * @param buttonName
+	 * 
+	 *                   the name of the desired button
+	 * 
+	 * @param index      Of Button
+	 * 
+	 *                   the index of the button to be changed
+	 * 
+	 * @throws IndexOutOfBoundsException if {@code i < 0 || i >= n }.
+	 * 
+	 *                                   A method that helps the user create desired button with complete freedom
+	 *                                   while also updating and changing the text associated with 
+	 *                                   specific buttons.
+	 */
+
+	public void SetButton(String buttonName,ImageIcon image, int indexOfButton) throws IndexOutOfBoundsException {
+		try {
+			pics[indexOfButton].setText(buttonName);
+			pics[indexOfButton].setIcon(image);
+	
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("The index you have entered is invalid");
+		}
+	}
+	
+	
+
+	/**
+	 * 
 	 * play the sound based on the pressed button
+	 * 
 	 */
+
 	public static void panelUpdate(JButton b, String buttonName) {
 
 	}
 
 	/**
+	 * 
 	 * A main method to test the view of the simulator app
+	 * 
 	 */
+
 	public static void main(String[] args) {
 
-		new SimulatorFrame(null, 1);
+		SimulatorFrame s = new SimulatorFrame(null, 4);
+
+		s.SetName("Happy", 0);
+		s.SetName("Sad", 1);
+		s.SetName("Angry", 2);
+		s.SetName("Perplexed", 3);
+		
+		s.SetImage(new ImageIcon("Happy.png"), 0);
+		s.SetImage(new ImageIcon("https://thumbs.dreamstime.com/z/perplexed-expression-real-man-50490656.jpg"), 1);
+		s.SetImage(new ImageIcon("https://www.improvisedlife.com/cms/wp-content/uploads/2017/11/angry-emoji-1.jpg"), 2);
+		s.SetImage(new ImageIcon("Perplexed.png"), 3);
 
 	}
 
