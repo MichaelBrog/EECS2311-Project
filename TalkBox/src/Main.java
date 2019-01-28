c34dae5869f2914199b03f39041551d8ebf5093e
mostafammagdy <mostafa.magdy.moussa@gmail.com>
2019-01-28 10:41:58 -0600

    test

--- TalkBox/src/Main.java
+++ TalkBox/src/Main.java
@@ -9,14 +9,13 @@ public class Main {
 		System.out.println("Hello Darkness my old friend");
 		System.out.println("NOPE");
 		System.out.println("NOPE");
-<<<<<<< HEAD
+
 		System.out.println("NOPE");
 		System.out.println("mostafa");
 	
-=======
-		System.out.println("hi");
+
 	}
->>>>>>> branch 'master' of https://github.com/ryang-123/EECS2311-Project
+
 
 }
-}
+
