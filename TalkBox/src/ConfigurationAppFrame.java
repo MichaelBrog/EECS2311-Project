3c9400623a9481bbe5421aee3380d3af6bba9feb
Nina <nina27@my.yorku.ca>
2019-02-16 11:29:34 -0500

    change

--- TalkBox/src/ConfigurationAppFrame.java
+++ TalkBox/src/ConfigurationAppFrame.java
@@ -8,7 +8,7 @@
  * */
 
 
-import java.awt.Container;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
 import java.util.Scanner;
--- TalkBox/src/ConfigurationListener.java
+++ TalkBox/src/ConfigurationListener.java
@@ -1,7 +1,7 @@
 /*
  * write object/ read object
  * */
-
+ 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
