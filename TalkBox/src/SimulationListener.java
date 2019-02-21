02c0e56aafe6ae3962c64d41157c78066058187c
ryang-123 <ryanguido29@gmail.com>
2019-02-21 10:04:38 -0500

    Play audio functionality tested and works when button is clicked

--- TalkBox/src/SimulationListener.java
+++ TalkBox/src/SimulationListener.java
@@ -2,6 +2,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.sound.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
@@ -27,19 +33,65 @@ public class SimulationListener implements ActionListener{
 	
 	
 	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		if (e.getSource() == "Hi")
+
+	public static void playMusic(String musicLocation)
+	{
+	
+		try
 		{
-			// TODO Auto-generated method stub
-			// open the sound file as a Java input stream
-		    String file = "Users\\ryann\\git\\EECS2311-Project\\EECS2311-Project\\TalkBox\\src\\hello.mp3";
-		    String music = "hello.mp3";
-		    Media hit = new Media(new File(music).toURI().toString());
-		    MediaPlayer mediaPlayer = new MediaPlayer(hit);
-		    mediaPlayer.play();
+			File musicPath = new File(musicLocation);
+			if (musicPath.exists())
+			{
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+				Clip clip = AudioSystem.getClip();
+				clip.open(audioInput);
+				clip.start();
+				
+				JOptionPane.showMessageDialog(null, "press okay to stop playing");
+			}
+			else
+			{
+				System.out.println("Can't find file");
+			}
 		}
+		
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+
+	@Override
+	
+	
+
+	public void actionPerformed(ActionEvent e) {
+
+		
+
+//		if (e.getSource() == "Hi")
+//
+//		{
+//
+//			// TODO Auto-generated method stub
+//
+//			// open the sound file as a Java input stream
+//
+//		    String file = "Users\\ryann\\git\\EECS2311-Project\\EECS2311-Project\\TalkBox\\src\\hello.mp3";
+//
+//		    String music = "hello.mp3";
+//
+//		    Media hit = new Media(new File(music).toURI().toString());
+//
+//		    MediaPlayer mediaPlayer = new MediaPlayer(hit);
+//
+//		    mediaPlayer.play();
+//
+//		}
+		
+		String filePath = "test.wav";
+		playMusic(filePath);
 		
 		
 	}
--- TalkBox/src/SimulatorFrame.java
+++ TalkBox/src/SimulatorFrame.java
@@ -1,77 +1,259 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
+
 import java.awt.GridLayout;
+
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-import javax.swing.border.*;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+import java.io.File;
 
 public class SimulatorFrame extends JFrame {
+
 	/**
-	 * A visual representation of simulation app
-	 */
 	
+	 * 
+	
+	 */
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 * 
+	 * 
+	 * A visual representation of simulation app
+	 * 
+	 * 
+	 * 
+	 */
 
 	// ------------ Fields ---------------------
+
 	JButton[] pics; // An array of buttons. Size initialized by the desired amount of buttons
-	JPanel panel;   // The used panel
+
+	JPanel panel; // The used panel
+
 	// -----------------------------------------
 
 	public SimulatorFrame(ActionListener l, int n) {
+
 		super("FrameDemo");
-		
+
 		initializePanel(l, n);
-		//frame.getContentPane().setBackground(Color.cyan);
+
+		// frame.getContentPane().setBackground(Color.cyan);
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setPreferredSize(new Dimension(1100, 600));
+
+		setPreferredSize(new Dimension(1100, 300));
+
 		pack();
+
 		setLocationRelativeTo(null);
+
 		setVisible(true);
+
 	}
 
 	/**
-	 * @param n
-	 * 			The number of buttons to be declared
-	 * @param l	
-	 * 			The action listener
 	 * 
-	 * A method that initialized the panel and provide the first view for the client
-	 * Associate the action listener l to the buttons --- GridLayout ? -----
+	 * 
+	 * 
+	 * @param n
+	 * 
+	 * 
+	 * 
+	 *          The number of buttons to be declared
+	 * 
+	 * 
+	 * 
+	 * @param l
+	 * 
+	 * 
+	 * 
+	 *          The action listener
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 *          A method that initialized the panel and provide the first view for
+	 * 
+	 *          the client
+	 * 
+	 * 
+	 * 
+	 *          Associate the action listener l to the buttons --- GridLayout ?
+	 * 
+	 *          -----
+	 * 
+	 * 
+	 * 
+	 * @param n The number of buttons to be declared
+	 * 
+	 * @param l The action listener
+	 * 
+	 * 
+	 * 
+	 *          A method that initialized the panel and provide the first view for
+	 * 
+	 *          the client Associate the action listener l to the buttons ---
+	 * 
+	 *          GridLayout ? ----- >>>>>>> branch 'master' of
+	 * 
+	 *          https://github.com/ryang-123/EECS2311-Project
+	 * 
 	 */
+
 	private void initializePanel(ActionListener l, int n) {
+
 		pics = new JButton[n];
+
 		panel = new JPanel(new GridLayout(1, n, 1, 1));
-		String [] actionDescription = {"Hi","Bye","Happy","Sad"};
-		
-		for (int i = 0; i < pics.length; i ++) {
-			
-			pics[i] = new JButton(actionDescription[i]);
-		//	pics[i].setBackground(Color.BLUE);
+
+		for (int i = 0; i < pics.length; i++) {
+
+			pics[i] = new JButton("");
+
+//			pics[i].setBackground(Color.BLUE);
+
+			pics[i].setVerticalTextPosition(SwingConstants.TOP);
+
+			pics[i].setHorizontalTextPosition(SwingConstants.CENTER);
+
 			pics[i].setVisible(true);
+
 			pics[i].addActionListener(l);
+
 			panel.add(pics[i]);
+
 		}
-		
+
 		this.add(panel);
+
 	}
 
 	/**
-	 * play the sound based on the pressed button
+	 * 
+	 * 
+	 * 
+	 * @param buttonName
+	 * 
+	 * 
+	 * 
+	 *                   the name of the desired button
+	 * 
+	 * 
+	 * 
+	 * @param image    
+	 * 
+	 * 					 The URL or link to the image
+	 * 
+	 * @param indexOfButton
+	 * 
+	 * 					 The index of the intended button
+	 * 
+	 * 
+	 * 
+	 * @throws IndexOutOfBoundsException if {@code i < 0 || i >= n }.
+	 * 
+	 * 
+	 * 
+	 *                                   A method that helps the user create desired
+	 * 
+	 *                                   button with complete freedom while also
+	 * 
+	 *                                   updating and changing the text associated
+	 * 
+	 *                                   with specific buttons.
+	 * 
 	 */
+
+	public void SetButton(String buttonName, String image, int indexOfButton) throws IndexOutOfBoundsException {
+
+		try {
+
+
+			ImageIcon icon = new ImageIcon(image);
+			
+			
+			
+			
+
+			pics[indexOfButton].setText(buttonName);
+			System.out.println("here");
+
+			pics[indexOfButton].setIcon(icon);
+			pics[indexOfButton].addActionListener(new SimulationListener(4));
+			
+			
+		} catch (IndexOutOfBoundsException e) {
+
+			System.out.println("The index you have entered is invalid");
+
+		}
+
+	}
+
+	/**
+	 * 
+	 * 
+	 * 
+	 * play the sound based on the pressed button
+	 * 
+	 * 
+	 * 
+	 */
+
 	public static void panelUpdate(JButton b, String buttonName) {
 
 	}
 
 	/**
+	 * 
+	 * 
+	 * 
 	 * A main method to test the view of the simulator app
+	 * 
+	 * 
+	 * 
 	 */
+
 	public static void main(String[] args) {
+
+		SimulatorFrame s = new SimulatorFrame(null, 4);
+
+		s.SetButton("Happy", "C:\\Users\\ryann\\git\\EECS2311-Project\\EECS2311-Project\\TalkBox\\src\\Happy.jpg", 0);
+
+		s.SetButton("Sad", "C:\\Users\\ryann\\git\\EECS2311-Project\\EECS2311-Project\\TalkBox\\src\\Sad.jpg",
+
+				1);
+
+		s.SetButton("Angry","C:\\Users\\ryann\\git\\EECS2311-Project\\EECS2311-Project\\TalkBox\\src\\Angry.jpg", 2);
+
+				//new ImageIcon("https://www.improvisedlife.com/cms/wp-content/uploads/2017/11/angry-emoji-1.jpg"), 2);
+		//s.SetButton("Perplexed", new ImageIcon("Perplexed.png"), 3);
+	//	SimulatorFrame s = new SimulatorFrame(null, 4);
+	//	s.SetButton("Happy", new ImageIcon("Happy.png"), 0);
+	//	s.SetButton("Sad",new ImageIcon("https://thumbs.dreamstime.com/z/perplexed-expression-real-man-50490656.jpg"),  1);
+	//	s.SetButton("Angry",new ImageIcon("https://www.improvisedlife.com/cms/wp-content/uploads/2017/11/angry-emoji-1.jpg"), 2);
+	//	s.SetButton("Perplexed",new ImageIcon("Perplexed.png"), 3);
+
+
+				//"C:\\Users\\mostafa\\git\\EECS2311-Project\\TalkBox\\src\\Angry.jpg", 2);
+
+		s.SetButton("Perplexed","C:\\Users\\ryann\\git\\EECS2311-Project\\EECS2311-Project\\TalkBox\\src\\Perplexed.jpg", 3);
+
 	
 
-		new SimulatorFrame(null, 4);
 
 	}
-
 }
