1af405954fa915070ca1ec0a5d796e9b03432cff
ryang-123 <ryanguido29@gmail.com>
2019-02-21 10:15:27 -0500

    Updated simulationlistener and simulatorframe to allow for the images to populate and play audio when clicked

--- TalkBox/src/SimulationListener.java
+++ TalkBox/src/SimulationListener.java
@@ -1,126 +1,103 @@
 import java.awt.event.ActionEvent;
-
 import java.awt.event.ActionListener;
-
 import java.io.File;
 
-
+import javax.sound.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 import javafx.scene.media.Media;
-
 import javafx.scene.media.MediaPlayer;
 
-
-
 public class SimulationListener implements ActionListener{
-
 	/**
-
 	 * Implement the Action listener of the pressed buttons/ check box in the configuration app
-
 	 * */
-
 	//------------------ Fields --------------------------
-
 	SimulatorFrame simFrame;
-
 	
-
 	//----------------------------------------------------
-
 	
-
 	/**
-
 	 * @param n
-
 	 * 			The number of buttons in the panel
-
 	 * A constructor that calls and initialized the configuration app frame with the current
-
 	 * action listener
-
 	 * */
-
 	public SimulationListener (int n) {
-
 		simFrame = new SimulatorFrame(this, n);
-
 	}
-
 	
-	public SimulationListener(SimulatorFrame sf)
+	
+	
+	
+
+	public static void playMusic(String musicLocation)
 	{
+	
+		try
+		{
+			File musicPath = new File(musicLocation);
+			if (musicPath.exists())
+			{
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+				Clip clip = AudioSystem.getClip();
+				clip.open(audioInput);
+				clip.start();
+				
+				JOptionPane.showMessageDialog(null, "press okay to stop playing");
+			}
+			else
+			{
+				System.out.println("Can't find file");
+			}
+		}
 		
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
-	
-	
-	
-	
-//	@Override
-//	public void actionPerformed(ActionEvent e) {
-//		
-//		if (e.getActionCommand() == "Happy")
-//		{
-//			// TODO Auto-generated method stub
-//			// open the sound file as a Java input stream
-//			System.out.println("Happy");
-//		    String file = "Users\\ryann\\git\\EECS2311-Project\\EECS2311-Project\\TalkBox\\src\\Battery.mp3";
-//		    String music = "Battery.mp3";
-//		    Media hit = new Media(new File(music).toURI().toString());
-//		    MediaPlayer mediaPlayer = new MediaPlayer(hit);
-//		    mediaPlayer.play();
-//		}
-//		
-//		
-//	}
-
-	
-
-	
-
 	
 
 	@Override
+	
+	
 
 	public void actionPerformed(ActionEvent e) {
 
 		
 
-		if (e.getSource() == "Hi")
-
-		{
-
-			// TODO Auto-generated method stub
-
-			// open the sound file as a Java input stream
-
-		    String file = "Users\\ryann\\git\\EECS2311-Project\\EECS2311-Project\\TalkBox\\src\\hello.mp3";
-
-		    String music = "hello.mp3";
-
-		    Media hit = new Media(new File(music).toURI().toString());
-
-		    MediaPlayer mediaPlayer = new MediaPlayer(hit);
-
-		    mediaPlayer.play();
-
-		}
-
+//		if (e.getSource() == "Hi")
+//
+//		{
+//
+//			// TODO Auto-generated method stub
+//
+//			// open the sound file as a Java input stream
+//
+//		    String file = "Users\\ryann\\git\\EECS2311-Project\\EECS2311-Project\\TalkBox\\src\\hello.mp3";
+//
+//		    String music = "hello.mp3";
+//
+//		    Media hit = new Media(new File(music).toURI().toString());
+//
+//		    MediaPlayer mediaPlayer = new MediaPlayer(hit);
+//
+//		    mediaPlayer.play();
+//
+//		}
 		
-
+		String filePath = "test.wav";
+		playMusic(filePath);
 		
-
+		
 	}
-
+	
+	
 	
 
 	
-
-	
-
-
-
-	
-
 }
