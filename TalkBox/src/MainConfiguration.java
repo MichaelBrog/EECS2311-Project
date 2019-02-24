ce7d06b7ed6c053b0d33673fce1170affb7d8ce3
Nina1234y <43324461+Nina1234y@users.noreply.github.com>
2019-02-24 03:00:47 -0500

    Update MainConfiguration.java

--- TalkBox/src/MainConfiguration.java
+++ TalkBox/src/MainConfiguration.java
@@ -1,21 +1,17 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.CodeSource;
 import java.util.stream.Collectors;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 
 import junit.framework.Test;
 
@@ -24,25 +20,43 @@ public class MainConfiguration {
 	public static void main(String[] args) {
 		String homeDirectory = System.getProperty("user.dir");
 		String decodedPath = "";
-		String saved_image_path_W = "\\Talk_Box_Data\\Images"; // windows
-		String saved_image_path_M = "/Talk_Box_Data/Images";	// mac/ linux/ unix
-		String saved_audio_path_W = "\\Talk_Box_Data\\Sound"; // windows
-		String saved_audio_path_M = "/Talk_Box_Data/Sound";	// mac/ linux/ unix
-		String serialized_data_folder_W = "\\Talk_Box_Data\\Serialized_Data";
-		String serialized_data_folder_M = "/Talk_Box_Data/Serialized_Data";
+		String saved_image_path_W = "\\imageReasource\\imageRepository"; // windows
+		String saved_image_path_M = "/imageReasource/imageRepository";	// mac/ linux/ unix
+		String saved_audio_path_W = "\\imageReasource\\soundRepository"; // windows
+		String saved_audio_path_M = "/imageReasource/soundRepository";	// mac/ linux/ unix
+		String serialized_data_folder_W = "\\imageReasource\\TalkBoxData";
+		String serialized_data_folder_M = "/imageReasource/TalkBoxData";
 
 
 		//input stream example
 		
-		String path = Test.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		/*String path = Test.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 		try {
 			decodedPath = URLDecoder.decode(path, "UTF-8");
 		} catch (UnsupportedEncodingException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
+*/
 		
+//		InputStream inputStream = MainConfiguration.class.getResourceAsStream("/hello.txt");
+//		
+//		String result = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+//		System.out.println(result);
+//		
+//		try {
+//			Files.createDirectories(Paths.get(homeDirectory + FilePathResource.REL_FILE_PATH));
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//		}
+		//create a class called filepathreasources
+//		try {
+//			
+//			Files.createDirectories(Paths.get(decodedPath + "//test"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
 		
 		CodeSource codeSource = MainConfiguration.class.getProtectionDomain().getCodeSource();
 		File jarFile = null;
@@ -60,11 +74,62 @@ public class MainConfiguration {
 				Files.createDirectories(Paths.get(jarDir + saved_image_path_W));
 				Files.createDirectories(Paths.get(jarDir + saved_audio_path_W));
 				Files.createDirectories(Paths.get(jarDir + serialized_data_folder_W));
+				
+				printfiles("thankyou1.wav", jarDir + saved_audio_path_W + "\\" + "thankyou1.wav");
+				printfiles("thankyou2.wav", jarDir + saved_audio_path_W + "\\" + "thankyou2.wav");
+				printfiles("thankyou3.wav", jarDir + saved_audio_path_W + "\\" + "thankyou3.wav");
+				printfiles("thankyou4.wav", jarDir + saved_audio_path_W + "\\" + "thankyou4.wav");
+				printfiles("thankyou5.wav", jarDir + saved_audio_path_W + "\\" + "thankyou5.wav");
+				printfiles("thankyou6.wav", jarDir + saved_audio_path_W + "\\" + "thankyou6.wav");
+				printfiles("thankyou7.wav", jarDir + saved_audio_path_W + "\\" + "thankyou7.wav");
+				printfiles("bye.wav", jarDir + saved_audio_path_W + "\\" + "bye.wav");
+				printfiles("happy.wav", jarDir + saved_audio_path_W + "\\" + "happy.wav");
+				printfiles("hello.wav", jarDir + saved_audio_path_W + "\\" + "hello.wav");
+				printfiles("no.wav", jarDir + saved_audio_path_W + "\\" + "no.wav");
+				printfiles("yes.wav", jarDir + saved_audio_path_W + "\\" + "yes.wav");
+				
+				printfiles("Happy.jpg", jarDir + saved_image_path_W + "\\" + "Happy.jpg");
+				printfiles("Sad.jpg", jarDir + saved_image_path_W + "\\" + "Sad.jpg");
+				printfiles("Perplexed.jpg", jarDir + saved_image_path_W + "\\" + "Perplexed.jpg");
+				printfiles("Angry.jpg", jarDir + saved_image_path_W + "\\" + "Angry.jpg");
 			}
 			else {
 				Files.createDirectories(Paths.get(jarDir + saved_image_path_M));
 				Files.createDirectories(Paths.get(jarDir + saved_audio_path_M));
 				Files.createDirectories(Paths.get(jarDir + serialized_data_folder_M));
+			
+				printfiles("thankyou1.wav", jarDir + saved_audio_path_M + "/" + "thankyou1.wav");
+				printfiles("thankyou2.wav", jarDir + saved_audio_path_M + "/" + "thankyou2.wav");
+				printfiles("thankyou3.wav", jarDir + saved_audio_path_M + "/" + "thankyou3.wav");
+				printfiles("thankyou4.wav", jarDir + saved_audio_path_M + "/" + "thankyou4.wav");
+				printfiles("thankyou5.wav", jarDir + saved_audio_path_M + "/" + "thankyou5.wav");
+				printfiles("thankyou6.wav", jarDir + saved_audio_path_M + "/" + "thankyou6.wav");
+				printfiles("thankyou7.wav", jarDir + saved_audio_path_M + "/" + "thankyou7.wav");
+				printfiles("bye.wav", jarDir + saved_audio_path_M + "/" + "bye.wav");
+				printfiles("happy.wav", jarDir + saved_audio_path_M + "/" + "happy.wav");
+				printfiles("hello.wav", jarDir + saved_audio_path_M + "/" + "hello.wav");
+				printfiles("no.wav", jarDir + saved_audio_path_M + "/" + "no.wav");
+				printfiles("yes.wav", jarDir + saved_audio_path_M + "/" + "yes.wav");
+				
+				printfiles("Happy.jpg", jarDir + saved_image_path_M + "/" + "Happy.jpg");
+				printfiles("Sad.jpg", jarDir + saved_image_path_M + "/" + "Sad.jpg");
+				printfiles("Perplexed.jpg", jarDir + saved_image_path_M + "/" + "Perplexed.jpg");
+				printfiles("Angry.jpg", jarDir + saved_image_path_M + "/" + "Angry.jpg");
+				
+				
+				
+			/*	File[] directoryListing = jarFile.listFiles();
+				if (directoryListing != null) {
+				    for (File child : directoryListing) {
+				    	if (child.toString().contains(".jpg"))
+				    		printfiles(child.toString(), jarDir + saved_image_path_M + "/" + child);
+				    	else if (child.toString().contains(".wav"))
+				    		printfiles(child.toString(), jarDir + saved_audio_path_M + "/" + child);
+						
+				    	}
+				    }*/
+						
+				
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -75,17 +140,16 @@ public class MainConfiguration {
 		conf.confFrame.pack();
 		
 		
-		printfiles("bye.wav", jarDir + saved_image_path_W + "/bye.wav");
+		//input stream example
+
 		
-		
-		
-		
+		//new File("./names.txt");
+
 		
 		
 		
 	}
 	
-
 	public static void printfiles(String resourcePath, String file_location) {
 	    try {
 	        InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream(resourcePath);
