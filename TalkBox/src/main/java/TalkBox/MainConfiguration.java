aac5ff6c56ee2cc44f4ace3ba77671e0c1673c26
MichaelBrog <mbrog@my.yorku.ca>
2019-04-01 19:57:18 -0400

    Added fileLoader Class
    The fileLoader class loads the default files from the jar file
    into the imageReasource folder

--- TalkBox/src/main/java/TalkBox/MainConfiguration.java
+++ TalkBox/src/main/java/TalkBox/MainConfiguration.java
@@ -39,111 +39,40 @@ public class MainConfiguration {
 		File jarFile = null;
 		Path index = null;
 		
-		try {
-			jarFile = new File(codeSource.getLocation().toURI().getPath());
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		String jarDir = jarFile.getParentFile().getPath();
-
-		
-		if (System.getProperty("os.name").startsWith("Windows")) 
-			index = Paths.get(jarDir + serialized_data_folder_W);
-		else
-			index = Paths.get(jarDir + serialized_data_folder_M);
-
-
-		    try {
-				Files.walk(index)
-				     .sorted(Comparator.reverseOrder())  // as the file tree is traversed depth-first and that deleted dirs have to be empty  
-				     .forEach(t -> {
-				         try {
-				             Files.delete(t);
-				         } catch (IOException e) {
-				         }
-				     });
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		    if (!Files.exists(index)) {
-		        try {
-					index = Files.createDirectories(index);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		    }
-		
-		
-		
-		
-		
-		/*try {
-			if (System.getProperty("os.name").startsWith("Windows")) {
-				Files.createDirectories(Paths.get(jarDir + saved_image_path_W));
-				Files.createDirectories(Paths.get(jarDir + saved_audio_path_W));
-				Files.createDirectories(Paths.get(jarDir + serialized_data_folder_W));
-				
-				printfiles("thankyou1.wav", jarDir + saved_audio_path_W + "\\" + "thankyou1.wav");
-				printfiles("thankyou2.wav", jarDir + saved_audio_path_W + "\\" + "thankyou2.wav");
-				printfiles("thankyou3.wav", jarDir + saved_audio_path_W + "\\" + "thankyou3.wav");
-				printfiles("thankyou4.wav", jarDir + saved_audio_path_W + "\\" + "thankyou4.wav");
-				printfiles("thankyou5.wav", jarDir + saved_audio_path_W + "\\" + "thankyou5.wav");
-				printfiles("thankyou6.wav", jarDir + saved_audio_path_W + "\\" + "thankyou6.wav");
-				printfiles("thankyou7.wav", jarDir + saved_audio_path_W + "\\" + "thankyou7.wav");
-				printfiles("bye.wav", jarDir + saved_audio_path_W + "\\" + "bye.wav");
-				printfiles("happy.wav", jarDir + saved_audio_path_W + "\\" + "happy.wav");
-				printfiles("hello.wav", jarDir + saved_audio_path_W + "\\" + "hello.wav");
-				printfiles("no.wav", jarDir + saved_audio_path_W + "\\" + "no.wav");
-				printfiles("yes.wav", jarDir + saved_audio_path_W + "\\" + "yes.wav");
-				
-				printfiles("Happy.jpg", jarDir + saved_image_path_W + "\\" + "Happy.jpg");
-				printfiles("Sad.jpg", jarDir + saved_image_path_W + "\\" + "Sad.jpg");
-				printfiles("Perplexed.jpg", jarDir + saved_image_path_W + "\\" + "Perplexed.jpg");
-				printfiles("Angry.jpg", jarDir + saved_image_path_W + "\\" + "Angry.jpg");
-			}
-			else {
-				Files.createDirectories(Paths.get(jarDir + saved_image_path_M));
-				Files.createDirectories(Paths.get(jarDir + saved_audio_path_M));
-				Files.createDirectories(Paths.get(jarDir + serialized_data_folder_M));
-			
-				printfiles("thankyou1.wav", jarDir + saved_audio_path_M + "/" + "thankyou1.wav");
-				printfiles("thankyou2.wav", jarDir + saved_audio_path_M + "/" + "thankyou2.wav");
-				printfiles("thankyou3.wav", jarDir + saved_audio_path_M + "/" + "thankyou3.wav");
-				printfiles("thankyou4.wav", jarDir + saved_audio_path_M + "/" + "thankyou4.wav");
-				printfiles("thankyou5.wav", jarDir + saved_audio_path_M + "/" + "thankyou5.wav");
-				printfiles("thankyou6.wav", jarDir + saved_audio_path_M + "/" + "thankyou6.wav");
-				printfiles("thankyou7.wav", jarDir + saved_audio_path_M + "/" + "thankyou7.wav");
-				printfiles("bye.wav", jarDir + saved_audio_path_M + "/" + "bye.wav");
-				printfiles("happy.wav", jarDir + saved_audio_path_M + "/" + "happy.wav");
-				printfiles("hello.wav", jarDir + saved_audio_path_M + "/" + "hello.wav");
-				printfiles("no.wav", jarDir + saved_audio_path_M + "/" + "no.wav");
-				printfiles("yes.wav", jarDir + saved_audio_path_M + "/" + "yes.wav");
-				
-				printfiles("Happy.jpg", jarDir + saved_image_path_M + "/" + "Happy.jpg");
-				printfiles("Sad.jpg", jarDir + saved_image_path_M + "/" + "Sad.jpg");
-				printfiles("Perplexed.jpg", jarDir + saved_image_path_M + "/" + "Perplexed.jpg");
-				printfiles("Angry.jpg", jarDir + saved_image_path_M + "/" + "Angry.jpg");
-				
-				
-				
-				File[] directoryListing = jarFile.listFiles();
-				if (directoryListing != null) {
-				    for (File child : directoryListing) {
-				    	if (child.toString().contains(".jpg"))
-				    		printfiles(child.toString(), jarDir + saved_image_path_M + "/" + child);
-				    	else if (child.toString().contains(".wav"))
-				    		printfiles(child.toString(), jarDir + saved_audio_path_M + "/" + child);
-						
-				    	}
-				    }
-						
-				
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		*/
+//		try {
+//			jarFile = new File(codeSource.getLocation().toURI().getPath());
+//		} catch (URISyntaxException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		String jarDir = jarFile.getParentFile().getPath();
+//
+//		
+//		if (System.getProperty("os.name").startsWith("Windows")) 
+//			index = Paths.get(jarDir + serialized_data_folder_W);
+//		else
+//			index = Paths.get(jarDir + serialized_data_folder_M);
+//
+//
+//		    try {
+//				Files.walk(index)
+//				     .sorted(Comparator.reverseOrder())  // as the file tree is traversed depth-first and that deleted dirs have to be empty  
+//				     .forEach(t -> {
+//				         try {
+//				             Files.delete(t);
+//				         } catch (IOException e) {
+//				         }
+//				     });
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		    if (!Files.exists(index)) {
+//		        try {
+//					index = Files.createDirectories(index);
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//		    }
 		
 		ConfigurationListener conf = new ConfigurationListener();
 		conf.confFrame.setVisible(true);
